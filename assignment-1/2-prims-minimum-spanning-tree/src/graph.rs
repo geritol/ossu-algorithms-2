730f4e10ed9c83f4ea02f882b78a35aa1cf7e8d5
Gergő Tolnai <gergo.tolnai@emarsys.com>
2019-05-11 12:12:52 +0200

    feat(1/prims-msp): mvp of `get_lowest_cost_edge`

--- assignment-1/2-prims-minimum-spanning-tree/src/graph.rs
+++ assignment-1/2-prims-minimum-spanning-tree/src/graph.rs
@@ -20,9 +20,14 @@ impl Graph {
             Some(node) => node.edges.push(edge),
             None => self.add_node(from, edge)
         };
+        let edge_2 = Edge{weight, target: from};
+        match self.nodes.get_mut(&to) {
+            Some(node) => node.edges.push(edge_2),
+            None => self.add_node(to, edge_2)
+        };
     }
 
-    pub fn get_lowest_cost_edge(&self, explored_nodes: &Vec<i32>) -> Option<Edge> {
+    pub fn get_lowest_cost_edge(&self, explored_nodes: &Vec<i32>) -> (i32, Option<Edge>) {
         let mut lowest_cost_edge: Option<Edge> = None;
         let mut lowest_cost_edge_start: Option<&i32> = None;
         for node_index in explored_nodes.iter() {
@@ -37,22 +42,14 @@ impl Graph {
                 }
             }
         }
-        // self.nodes.get_mut(lowest_cost_edge_start.unwrap()).unwrap().edges.pop();
-        lowest_cost_edge
+        (*lowest_cost_edge_start.unwrap() ,lowest_cost_edge)
     }
 
     fn get_lowest_unvisited_edge(&self, node_index: &i32, explored_nodes: &Vec<i32>) -> Option<Edge>{
         let lowest_edge = self.nodes.get(node_index).unwrap().edges.peek();
         match lowest_edge {
             None => None,
-            Some(edge) => {
-                if explored_nodes.contains(&edge.target) {
-                    // self.nodes.get_mut(node_index).unwrap().edges.pop();
-                    self.get_lowest_unvisited_edge(node_index, explored_nodes)
-                } else {
-                    Some(edge.clone())
-                }
-            }
+            Some(edge) => Some(edge.clone())
         }
         
     }
@@ -85,7 +82,27 @@ mod test {
         graph.add_edge(2,4, 4);
         graph.add_edge(3,1,3);
         graph.add_edge(4,3,5);
-        let visited = vec![1];
-        assert_eq!(graph.get_lowest_cost_edge(&visited).unwrap().weight, -1)
+        let mut visited = vec![1];
+
+        let first_result = graph.get_lowest_cost_edge(&visited);
+        assert_eq!(first_result.1.unwrap().weight, -1);
+        graph.nodes.get_mut(&first_result.0).unwrap().edges.pop();
+        visited.push(first_result.1.unwrap().target);
+
+        let mut second_result = graph.get_lowest_cost_edge(&visited);
+        if visited.contains(&second_result.1.unwrap().target) {
+            graph.nodes.get_mut(&second_result.0).unwrap().edges.pop();
+            second_result = graph.get_lowest_cost_edge(&visited);
+        }
+        assert_eq!(second_result.1.unwrap().weight, 2);
+        graph.nodes.get_mut(&second_result.0).unwrap().edges.pop();
+        visited.push(second_result.1.unwrap().target);
+
+        // let third_result = graph.get_lowest_cost_edge(&visited);
+        // assert_eq!(third_result.1.unwrap().weight, 3);
+        // graph.nodes.get_mut(&third_result.0).unwrap().edges.pop();
+        // visited.push(third_result.1.unwrap().target);
+
+        // assert_eq!(graph.get_lowest_cost_edge(&visited).unwrap().weight, 2)
     }
 }
