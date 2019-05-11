7b46774c39c83a8c166508f24fbf67346a1190d4
Gergő Tolnai <gergo.tolnai@emarsys.com>
2019-05-11 12:13:22 +0200

    feat(1/prims-msp): `get_lowest_unvisited_edge`

--- assignment-1/2-prims-minimum-spanning-tree/src/graph.rs
+++ assignment-1/2-prims-minimum-spanning-tree/src/graph.rs
@@ -27,25 +27,37 @@ impl Graph {
         };
     }
 
-    pub fn get_lowest_cost_edge(&self, explored_nodes: &Vec<i32>) -> (i32, Option<Edge>) {
+    pub fn get_lowest_unvisited_edge(&mut self, explored_nodes: &Vec<i32>) -> (Option<i32>, Option<Edge>) {
+        let result = &self.get_lowest_cost_edge(explored_nodes);
+        if result.0 == None {
+            return (None, None)
+        }
+        &self.nodes.get_mut(&result.0.unwrap()).unwrap().edges.pop();
+        if explored_nodes.contains(&result.1.unwrap().target) {
+            return self.get_lowest_unvisited_edge(explored_nodes)
+        }
+        *result
+    }
+
+    fn get_lowest_cost_edge(&self, explored_nodes: &Vec<i32>) -> (Option<i32>, Option<Edge>) {
         let mut lowest_cost_edge: Option<Edge> = None;
-        let mut lowest_cost_edge_start: Option<&i32> = None;
+        let mut lowest_cost_edge_start: Option<i32> = None;
         for node_index in explored_nodes.iter() {
-            let lowest_edge = self.get_lowest_unvisited_edge(node_index, explored_nodes);
+            let lowest_edge = self.get_copy_of_lowest_edge(node_index, explored_nodes);
             if lowest_edge != None {
                 if lowest_cost_edge == None {
                     lowest_cost_edge = lowest_edge;
-                    lowest_cost_edge_start = Some(node_index);
+                    lowest_cost_edge_start = Some(*node_index);
                 } else if lowest_edge > lowest_cost_edge {
                     lowest_cost_edge = lowest_edge;
-                    lowest_cost_edge_start = Some(node_index);
+                    lowest_cost_edge_start = Some(*node_index);
                 }
             }
         }
-        (*lowest_cost_edge_start.unwrap() ,lowest_cost_edge)
+        (lowest_cost_edge_start ,lowest_cost_edge)
     }
 
-    fn get_lowest_unvisited_edge(&self, node_index: &i32, explored_nodes: &Vec<i32>) -> Option<Edge>{
+    fn get_copy_of_lowest_edge(&self, node_index: &i32, explored_nodes: &Vec<i32>) -> Option<Edge>{
         let lowest_edge = self.nodes.get(node_index).unwrap().edges.peek();
         match lowest_edge {
             None => None,
@@ -84,25 +96,20 @@ mod test {
         graph.add_edge(4,3,5);
         let mut visited = vec![1];
 
-        let first_result = graph.get_lowest_cost_edge(&visited);
+        let first_result = graph.get_lowest_unvisited_edge(&visited);
         assert_eq!(first_result.1.unwrap().weight, -1);
-        graph.nodes.get_mut(&first_result.0).unwrap().edges.pop();
         visited.push(first_result.1.unwrap().target);
 
-        let mut second_result = graph.get_lowest_cost_edge(&visited);
-        if visited.contains(&second_result.1.unwrap().target) {
-            graph.nodes.get_mut(&second_result.0).unwrap().edges.pop();
-            second_result = graph.get_lowest_cost_edge(&visited);
-        }
+        let second_result = graph.get_lowest_unvisited_edge(&visited);
         assert_eq!(second_result.1.unwrap().weight, 2);
-        graph.nodes.get_mut(&second_result.0).unwrap().edges.pop();
         visited.push(second_result.1.unwrap().target);
 
-        // let third_result = graph.get_lowest_cost_edge(&visited);
-        // assert_eq!(third_result.1.unwrap().weight, 3);
-        // graph.nodes.get_mut(&third_result.0).unwrap().edges.pop();
-        // visited.push(third_result.1.unwrap().target);
+        let third_result = graph.get_lowest_unvisited_edge(&visited);
+        assert_eq!(third_result.1.unwrap().weight, 3);
+        visited.push(third_result.1.unwrap().target);
 
-        // assert_eq!(graph.get_lowest_cost_edge(&visited).unwrap().weight, 2)
+        let last_result = graph.get_lowest_unvisited_edge(&visited);
+        assert_eq!(last_result.0, None);
+        assert_eq!(last_result.1, None);
     }
 }
