5ebae1240bce0c46de0db3cccfa60926cfecc360
Gergő Tolnai <gergo.tolnai@emarsys.com>
2019-05-19 10:45:51 +0200

    feat(2/clustering): read file and sort edges

--- assignment-2/clustering/src/edge.rs
+++ assignment-2/clustering/src/edge.rs
@@ -3,19 +3,19 @@ use std::cmp::Ordering;
 #[derive(Eq, PartialEq, Clone, Copy, Debug)]
 pub struct Edge {
     pub weight: i32,
+    pub source: i32, // Source node's id
     pub target: i32, // Target node's id
 }
 
-// Reverse ordering
 impl Ord for Edge {
     fn cmp(&self, other: &Edge) -> Ordering {
-        other.weight.cmp(&self.weight)
+        self.weight.cmp(&other.weight)
     }
 }
 
 impl PartialOrd for Edge {
     fn partial_cmp(&self, other: &Edge) -> Option<Ordering> {
-        other.weight.partial_cmp(&self.weight)
+        self.weight.partial_cmp(&other.weight)
     }
 }
 
@@ -25,8 +25,8 @@ mod test {
 
     #[test]
     fn odering_edge_smaller() {
-        let negative_edge = Edge{weight: -1, target: 10};
-        let big_edge = Edge{weight: 10, target: 11};
-        assert_eq!(negative_edge > big_edge, true)
+        let negative_edge = Edge{weight: -1, target: 10, source: 1};
+        let big_edge = Edge{weight: 10, target: 11, source: 2};
+        assert_eq!(negative_edge > big_edge, false)
     }
 }
--- assignment-2/clustering/src/main.rs
+++ assignment-2/clustering/src/main.rs
@@ -1,5 +1,30 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 mod edge;
+use edge::Edge;
 
 fn main() {
-    println!("Hello, world!");
+    let mut edges = read_edges_from_file("clustering1.small.txt");
+    println!("{:?}", edges[0]);
+    edges.sort();
+    println!("{:?}", edges[0])
+}
+
+fn read_edges_from_file(file_name: &str) -> Vec<Edge>{
+    let file = File::open(file_name).expect("Unable to open");
+    let mut edges = Vec::new();
+    for (index, line) in BufReader::new(file).lines().enumerate() {
+        if index == 0 {
+            // TODO: better first line handling
+        } else {
+            let unwrapped_line = line.unwrap();
+            let split: Vec<&str> = unwrapped_line.split(' ').collect();
+            let source = split[0].parse::<i32>().unwrap();
+            let target = split[1].parse::<i32>().unwrap();
+            let weight = split[2].parse::<i32>().unwrap();
+            edges.push(Edge{source, target, weight});
+        }
+    }
+    edges
 }
